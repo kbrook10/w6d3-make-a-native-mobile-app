11bc912c8b802d26f38095f730c2dc2891777c2e
keith brooks <kbrook10@gmail.com>
2016-11-03 08:07:05 -0400

    Update components Greeting, TextInput and Image

--- index.ios.js
+++ index.ios.js
@@ -11,25 +11,51 @@ import {
   Text,
   View,
   Image,
-  MapView,
+  TextInput,
 } from 'react-native';
 
-export default class LectureApp extends Component {
+class Greeting extends Component {
   render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+export default class LectureApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Enter something special' };
+  }
+  render() {
+    let pic = {
+      uri: 'https://facebook.github.io/react/img/logo_og.png'
+    };
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to Indy!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          This is my first Mobile App!{'\n'}
         </Text>
-        <Text style={styles.instructions}>
+        {/* <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
+        </Text> */}
+        <Image source={pic} style={{width: 200, height: 200, borderRadius: 100}} />
+        <Text style={styles.welcome}>
+          {'\n'}
         </Text>
-        <Image source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
-       style={{width: 200, height: 200, borderRadius: 100}} />
+        <View style={{alignItems: 'center'}}>
+          <Greeting name='New User' />
+        </View>
+        <Text style={styles.welcome}>
+          {'\n'}
+        </Text>
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1, textAlign: 'center',}}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}/>
       </View>
     );
   }
